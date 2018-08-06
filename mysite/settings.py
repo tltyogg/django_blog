ff7f429f370ee1b437ef26656ea4c68ee190c0d9
tltyogg <saralyn.carbaugh@gmail.com>
2018-08-06 17:30:44 -0400

    static site issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,6 +124,4 @@ STATICFILES_FINDERS = [
 ]
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATIC_ROOT = '/home/scarbaugh/scarbaugh.pythonanywhere.com/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
