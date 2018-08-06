b5d89159e3e7e4d7ff15ee15db6e7a54090e9699
Sara Carbaugh <scarbaugh@24dcaea7d349>
2018-08-06 20:40:47 +0000

    setting update

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,7 @@ STATICFILES_FINDERS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
